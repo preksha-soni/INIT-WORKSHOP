e20ee93992cbaf17f8da56bb35e1379e08211892
preksha-soni <prekshasoni13@gmail.com>
2026-02-24 16:01:43 +0530

    cors fixed

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://127.0.0.1:5500",
-    "https://YOUR-NETLIFY-LINK.netlify.app"
+    "https://tubular-sunshine-21a65e.netlify.app"
   ],
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
